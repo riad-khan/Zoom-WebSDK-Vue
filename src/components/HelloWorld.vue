acef5299beca2b7f56d40018cf2fb687475c6125
riad-khan <rhkhan2367@gmail.com>
2021-06-03 09:26:40 +0600

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <main>
+    <zoom-join></zoom-join>
+  </main>
+</template>
+
+<script>
+import zoomJoin from './zoom-join.vue';
+// import axios from "axios";
+
+export default {
+  components: { zoomJoin },
+  name: 'HelloWorld',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+main {
+  width: 70%;
+  margin: auto;
+  text-align: center;
+}
+
+main button {
+  margin-top: 20px;
+  background-color: #2D8CFF;
+  color: #ffffff;
+  text-decoration: none;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-left: 40px;
+  padding-right: 40px;
+  display: inline-block;
+  border-radius: 10px;
+  cursor: pointer;
+  border: none;
+  outline: none;
+}
+
+main button:hover {
+  background-color: #2681F2;
+}
+</style>
---
+++ src/components/zoom-join.vue
@@ -0,0 +1,73 @@
+<template>
+     <button @click="startMeeting">Join Meeting</button>
+</template>
+
+<script>
+
+export default{
+  
+   
+  created () {
+    this.ZoomMtg.setZoomJSLib('https://source.zoom.us/1.9.5/lib', '/av');
+    this.ZoomMtg.preLoadWasm();
+    this.ZoomMtg.prepareJssdk();
+  },
+  mounted() {
+    this.ZoomMtg.inMeetingServiceListener("onUserJoin", (data) => {
+      console.log("inMeetingServiceListener onUserJoin", data);
+    });
+  },
+  data () {
+    return {
+      apiKey: "wuG25BXKSt6X33HXGk_d-A",
+      leaveUrl: "http://localhost:8080",
+      meetingNumber: "8131951687",
+      passWord: "2i6Zsi",
+      role: 1,
+      signatureEndpoint: "http://localhost:4000",
+      userEmail: "",
+      userName: "Vue.js"
+    }
+  },
+  methods: {
+    startMeeting() {
+      var signature='d3VHMjVCWEtTdDZYMzNIWEdrX2QtQS44MTMxOTUxNjg3LjE2MjI2NDQ1MTgwMDAuMC5POXJabE1mclE2dms3eHFDZ0hBdWNFR3pxa2JDVXJHRStrd1ZVNGZ3Nk9rPQ';
+      document.getElementById("zmmtg-root").style.display = "block";
+
+      this.ZoomMtg.init({
+        leaveUrl: this.leaveUrl,
+        isSupportAV: false,
+        success: (success) => {
+          console.log(success);
+          this.ZoomMtg.join({
+            meetingNumber: this.meetingNumber,
+            userName: this.userName,
+            signature: signature,
+            apiKey: this.apiKey,
+            userEmail: this.userEmail,
+            passWord: this.passWord,
+            success: (success) => {
+              console.log(success);
+            },
+            error: (error) => {
+              console.log(error);
+            }
+          });
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      });
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+body{
+  background: white;
+}
+
+</style>
